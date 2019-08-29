6c9325a2daf06d0e8cb4c2e96db19da7c8fcfbe9
Alex Sevy - ec2 <alex.sevy@gmail.com>
2019-08-29 18:49:19 +0000

    added some more unit tests - these are not compiled though

--- unit_test/TestMainWindow.cc
+++ unit_test/TestMainWindow.cc
@@ -31,9 +31,10 @@ void TestMainWindow::testGUI() {
 void TestMainWindow::testRunFromFileMenu() {
 	main = new MainWindow();
 
+	// Connect action handlers to check tab correctness after processing
 	connect( main, &MainWindow::verifySummaryTab,
-			 this, &TestMainWindow::checkSummaryTab );
-
+			 this, &TestMainWindow::verifyTabCorrectness );
+			
 	// Set up options
 	main->options_->nthreads( -1 );
 	main->options_->error_threshold( errorx::constants::OPTIMIZED_THRESHOLD );
@@ -45,15 +46,33 @@ void TestMainWindow::testRunFromFileMenu() {
 
 	// Wait for protocol to finish in increments of 250ms
 	while ( !executionDone ) QTest::qWait( 250 );
+
+
+	// Run the protocol again and make sure it's consistent
+	QTimer::singleShot( 1, this, &TestMainWindow::testFASTQDialog );
+		main->newProjectAction->trigger();
+
+		// Wait for protocol to finish in increments of 250ms
+		while ( !executionDone ) QTest::qWait( 250 );
+}
+
+void TestMainWindow::verifyTabCorrectness() {
+	executionDone = 1;
+
+	checkSummaryTab();
+	checkDataTab();
+	checkTableExport();
+	checkClonotypeTab();
+	checkClonotypeTableExport();
+	checkErrorTab();
+	checkGeneTab();
 }
 
 void TestMainWindow::checkSummaryTab() {
 
-	executionDone = 1;
-
 	QCOMPARE( main->summaryTab_->line2->text(), "100" );
 	QCOMPARE( main->summaryTab_->line3->text(), "79" );
-	QCOMPARE( main->summaryTab_->line4->text(), "59" );
+	QCOMPARE( main->summaryTab_->line4->text(), "48" );
 	QCOMPARE( main->summaryTab_->line5->text(), "79" );
 
 	auto it = main->summaryTab_->plotBars->data()->begin();
@@ -66,13 +85,142 @@ void TestMainWindow::checkSummaryTab() {
 
 	it++;
 	QCOMPARE( it->mainKey(), 2 );
-	QCOMPARE( it->mainValue(), 59 );
+	QCOMPARE( it->mainValue(), 48 );
 
 	it++;
-	QCOMPARE( it->mainKey(), 4 );
+	QCOMPARE( it->mainKey(), 3 );
 	QCOMPARE( it->mainValue(), 79 );
 
 	QCOMPARE( 0, 1 );
+}
+
+void TestMainWindow::checkDataTab() {
+	QTableWidget* table = main->dataTab_->mainTable;
+
+	// TODO create the correct table file
+	//
+	// First, check the short table and make sure it's correct
+	ifstream in ( "correct_table_short.tsv" );
+	string inputStr;
+	int colCount = 5; // TODO set correct colCount
+
+	// Get rid of the header
+	for ( int ii = 0; ii < colCount; ++ii ) in >> inputStr;
+	
+	QTableWidgetItem* item;
+	for ( int row_it = 0 ; row_it < table->rowCount(); ++row_it ) {
+		for ( int col_it = 0; col_it < table->columnCount(); ++col_it ) {
+			in >> inputStr;
+			item = table->item( row_it, col_it );
+			QCOMPARE( inputStr, item.text() );
+		}
+	}
+
+	// Next, activate the full table and make sure it's correct
+	main->dataTab_->checkBox->setSelected( true );
+	main->dataTab_->mainTable;
+	in ( "correct_table_long.tsv" );
+	colCount = 53; // TODO set correct colCount
+
+	// Get rid of the header
+	for ( int ii = 0; ii < colCount; ++ii ) in >> inputStr;
+	
+	for ( int row_it = 0 ; row_it < table->rowCount(); ++row_it ) {
+		for ( int col_it = 0; col_it < table->columnCount(); ++col_it ) {
+			in >> inputStr;
+			item = table->item( row_it, col_it );
+			QCOMPARE( inputStr, item.text() );
+		}
+	}
+}
+
+void TestMainWindow::checkTableExport() {
+	// TODO create correct data tables
+	
+	// Export first with full_data selected
+	main->dataTab_->checkBox->setSelected( true );
+	main->dataTab_->exportTable( "data_table_export.tsv" );
+	QCOMPARE( checksum( "data_table_export.tsv" ), 
+			  checksum( "correct_table_long.tsv" ));
+	QFile file( "data_table_export.tsv" );
+	file.remove();
+	
+	// Export first with full_data selected
+	main->dataTab_->checkBox->setSelected( false );
+	main->dataTab_->exportTable( "data_table_export.tsv" );
+	QCOMPARE( checksum( "data_table_export.tsv" ),
+			  checksum( "correct_table_long.tsv" ));
+	file.remove();
+}
+
+void TestMainWindow::checkClonotypeTab() {
+	QTableWidget* table = main->clonotypeTab_->cloneTable;
+
+	// TODO create the correct table file
+	ifstream in ( "correct_clonotype_table.tsv" );
+	string inputStr;
+	int colCount = 3;
+
+	// Get rid of the header
+	for ( int ii = 0; ii < colCount; ++ii ) in >> inputStr;
+
+	QTableWidgetItem* item;
+	for ( int row_it = 0 ; row_it < table->rowCount(); ++row_it ) {
+		for ( int col_it = 0; col_it < table->columnCount(); ++col_it ) {
+			in >> inputStr;
+			item = table->item( row_it, col_it );
+			QCOMPARE( inputStr, item.text() );
+		}
+	}
+}
+
+void TestMainWindow::checkClonotypeTableExport() {
+	// Export clonotype table
+	main->dataTab_->checkBox->setSelected( true );
+	main->dataTab_->exportTable( "clonotype_table_export.tsv" );
+	QCOMPARE( checksum( "clonotype_table_export.tsv" ),
+			  checksum( "correct_clonotype_table.tsv" ));
+	QFile file( "clonotype_table_export.tsv" );
+	file.remove();
+}
+
+void TestMainWindow::checkErrorTab() {
+
+	// TODO input correct values here
+	QCOMPARE( main->errorTab_->beforeErrorRate->text(), "100" );
+	QCOMPARE( main->errorTab_->afterErrorRate->text(), "100" );
+
+	auto it = main->errorTab_->plotBars->data()->begin();
+	std::vector<int> histogramValues = { /* TODO input histogram values */ };
+
+	for ( int ii = 0; ii < histogramValues.size(); ++ii ) {
+		QCOMPARE( it->mainKey(), ii );
+		QCOMPARE( it->mainValue(), histogramValues[ ii ] );
+
+		++it;
+	}
+}
+
+void TestMainWindow::checkGeneTab() {
+
+    auto it = main->geneTab_->vBars->data()->begin();
+    std::map<string,int> valuesV = { /* TODO input V gene values */ };
+	std::map<string,int> valuesJ = { /* TODO input J gene values */ };
+
+    for ( int ii = 0; ii < valuesV.size(); ++ii ) {
+        QCOMPARE( it->mainKey(), ii );
+        QCOMPARE( it->mainValue(), valuesV[ ii ] );
+
+        ++it;
+    }
+
+	it = main->geneTab_->jBars->data()->begin();
+	for ( int ii = 0; ii < valuesJ.size(); ++ii ) {
+        QCOMPARE( it->mainKey(), ii );
+        QCOMPARE( it->mainValue(), valuesJ[ ii ] );
+
+        ++it;
+    }
 }
  
 void TestMainWindow::testFASTQDialog() {
@@ -90,6 +238,17 @@ void TestMainWindow::testFASTQDialog() {
 	}
 }
 
+QByteArray checksum( QString const & fileName ) {
+	QCryptographicHash hash( QCryptographicHash::Sha1 );
+	QFile file( fileName );
+
+	if ( file.open( QIODevice::ReadOnly ) ) {
+		hash.addData( file.readAll() );
+	}
+
+	// Retrieve the SHA1 signature of the file
+	return hash.result();
+}
 
 QTEST_MAIN( TestMainWindow )
 #include "TestMainWindow.moc"
