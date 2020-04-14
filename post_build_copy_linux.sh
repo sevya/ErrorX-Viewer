82b40d38d2926a2010b533cf902a2202a94ba433
Alex Sevy <alex.sevy@gmail.com>
2020-04-14 11:04:11 -0400

    add patchelf

--- post_build_copy_linux.sh
+++ post_build_copy_linux.sh
@@ -2,5 +2,9 @@ patchelf --replace-needed liberrorx.so /usr/local/lib/liberrorx.so ErrorX_Viewer
 # rm -f ErrorX_Viewer.AppDir/AppRun
 # ~/linuxdeploy-x86_64.AppImage --appdir ErrorX_Viewer.AppImage --library lib/liberrorx.so --executable ErrorX_Viewer --desktop-file ErrorX_Viewer.desktop --icon-file helix_icon.png --output appimage
 # ~/linuxdeploy-x86_64.AppImage --appdir ErrorX_Viewer.AppDir --executable ErrorX_Viewer --desktop-file ErrorX_Viewer.desktop --icon-file helix_icon.png --output appimage
+mkdir ErrorX-Viewer_1.0.0
+mkdir ErrorX-Viewer_1.0.0/usr/
+mkdir ErrorX-Viewer_1.0.0/usr/local/
+mkdir ErrorX-Viewer_1.0.0/usr/local/bin/
 cp ErrorX_Viewer ErrorX-Viewer_1.0.0/usr/local/bin/
 # dpkg-deb --build ErrorX-Viewer_1.0.0
